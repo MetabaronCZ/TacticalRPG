2a6c4d493c485df35e9fc383fdbff12b0c6c07a7
MetabaronCZ <33844039+MetabaronCZ@users.noreply.github.com>
2018-05-08 16:31:41 +0200

    Update dependencies

--- src/scripts/reducers/index.ts
+++ src/scripts/reducers/index.ts
@@ -1,10 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers, ReducersMapObject } from 'redux';
 
 import { IStore } from 'store';
 import parties from 'reducers/parties';
 import characters from 'reducers/characters';
+import { ICharacterData } from 'models/character-data';
 
 export default combineReducers<IStore>({
 	characters,
 	parties
-});
+} as ReducersMapObject<IStore>);
