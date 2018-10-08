c678058fd55ebc0192ff11a4f4b7a872d873eee7
MetabaronCZ <33844039+MetabaronCZ@users.noreply.github.com>
2018-10-08 10:33:59 +0200

    Add mobx dev tools

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DevTools from 'mobx-react-devtools';
 
 import { Provider, context } from 'context';
 
@@ -11,6 +12,8 @@ ReactDOM.render(
 		<Provider value={context}>
 			<Router />
 		</Provider>
+
+		<DevTools />
 	</PageWrapper>,
 	document.querySelector('.PageContainer')
 );
