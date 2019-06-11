c94b96a063167e9245a034776a887ba62c5ec6a8
MetabaronCZ <33844039+MetabaronCZ@users.noreply.github.com>
2019-06-11 15:43:07 +0200

    Add unit tests for navigation

---
+++ tests/core/navigation.test.ts
@@ -0,0 +1,60 @@
+import { createMemoryHistory, MemoryHistory } from 'history';
+
+import paths from 'data/routes';
+import { RouteID } from 'modules/route';
+import { goto, gotoRoute, gotoFn } from 'core/navigation';
+
+const createHistory = () => createMemoryHistory({
+	initialEntries: [],
+	initialIndex: 0
+});
+
+const hasNavigated = (history: MemoryHistory, path: string): boolean => {
+	if (1 !== history.entries.length) {
+		return false;
+	}
+	return path === history.entries[0].pathname;
+};
+
+describe('core.navigation', () => {
+	describe('#goto', () => {
+		test('it navigates to given URL', () => {
+			const history = createHistory();
+			expect(history.entries.length).toEqual(0);
+
+			const path = '/myPage';
+			goto(history, path);
+
+			expect(hasNavigated(history, path)).toBeTruthy();
+		});
+	});
+
+	describe('#gotoRoute', () => {
+		test('it navigates to given route', () => {
+			const history = createHistory();
+			expect(history.entries.length).toEqual(0);
+
+			const route: RouteID = 'CHARACTER_LIST';
+			gotoRoute(history, route);
+
+			const path = paths[route];
+			expect(hasNavigated(history, path)).toBeTruthy();
+		});
+	});
+
+	describe('#gotoFn', () => {
+		test('it returns function which navigates to given route', () => {
+			const history = createHistory();
+			expect(history.entries.length).toEqual(0);
+
+			const route: RouteID = 'CHARACTER_LIST';
+			const fn = gotoFn(history, route);
+			expect(history.entries.length).toEqual(0);
+
+			fn();
+
+			const path = paths[route];
+			expect(hasNavigated(history, path)).toBeTruthy();
+		});
+	});
+});
---
+++ tests/modules/route.test.ts
@@ -0,0 +1,29 @@
+import paths from 'data/routes';
+import { getPath, RouteID } from 'modules/route';
+
+describe('modules.route', () => {
+	describe('#getPath', () => {
+		test('it returns right URL path to given route', () => {
+			const route: RouteID = 'CHARACTER_CREATE';
+			const path = paths[route];
+			const result = getPath(route);
+			expect(result).toEqual(path);
+		});
+
+		test('it applies given parameters correctly', () => {
+			const route: RouteID = 'PARTY_EDIT';
+			const path = paths[route];
+			expect(path).toMatch(/\:id/);
+
+			const res1 = getPath(route);
+			expect(res1).toEqual(path);
+
+			const res2 = getPath(route, '');
+			expect(res2).toEqual(path);
+
+			const param = 'x=10';
+			const res3 = getPath(route, param);
+			expect(res3).toEqual(res1.replace(':id', param));
+		});
+	});
+});
